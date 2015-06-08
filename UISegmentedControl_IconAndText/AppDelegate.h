b24f2f3c255d6f52b55d0edd253346b166d9499d
Maxim Bilan <maximb.mail@gmail.com>
2015-06-08 12:43:09 +0300

    Added project template

---
+++ UISegmentedControl_IconAndText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UISegmentedControl_IconAndText
+//
+//  Created by Maxim Bilan on 6/8/15.
+//  Copyright (c) 2015 Maxim Bilan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UISegmentedControl_IconAndText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UISegmentedControl_IconAndText
+//
+//  Created by Maxim Bilan on 6/8/15.
+//  Copyright (c) 2015 Maxim Bilan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
